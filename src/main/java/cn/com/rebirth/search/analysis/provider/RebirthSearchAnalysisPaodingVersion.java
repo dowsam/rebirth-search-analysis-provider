fb52575c7559cc2395b73845ef6b63cb7e8af28d
dowsam <l.xue.nong@gmail.com>
2012-09-05 13:55:45 +0800

    小度修改

--- src/main/java/cn/com/rebirth/search/analysis/provider/RebirthSearchAnalysisPaodingVersion.java
+++ src/main/java/cn/com/rebirth/search/analysis/provider/RebirthSearchAnalysisPaodingVersion.java
@@ -4,6 +4,7 @@
  */
 package cn.com.rebirth.search.analysis.provider;
 
+import cn.com.rebirth.commons.AbstractVersion;
 import cn.com.rebirth.commons.Version;
 
 /**
@@ -11,15 +12,10 @@ import cn.com.rebirth.commons.Version;
  *
  * @author l.xue.nong
  */
-public class RebirthSearchAnalysisPaodingVersion implements Version {
+public class RebirthSearchAnalysisPaodingVersion extends AbstractVersion implements Version {
 
 	/** The Constant serialVersionUID. */
 	private static final long serialVersionUID = -8043854155386241988L;
-
-	@Override
-	public String getModuleVersion() {
-		return "0.0.1.RC1-SNAPSHOT";
-	}
 
 	@Override
 	public String getModuleName() {
